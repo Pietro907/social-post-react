e00638a17588c8327ee9afa55c2d64b251a83cd5
Pietro907 <pietrop.puri@gmail.com>
2024-01-19 11:36:44 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import './Navbar.css';
+
+function Navbar() {
+
+    return (
+
+        <ul>
+            <li><a href="#">Menù</a></li>
+            <li><a href="#">Menù</a></li>
+            <li><a href="#">Menù</a></li>
+            <li><a href="#">Menù</a></li>
+            <li><a href="#">Menù</a></li>
+            <li><a href="#">Menù</a></li>
+        </ul>
+
+    )
+}
+
+export default Navbar
